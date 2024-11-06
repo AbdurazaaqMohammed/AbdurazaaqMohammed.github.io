a46599a8451eba5124f623a550a643833bfda984
AbdurazaaqMohammed <zaqasar@gmail.com>
2024-11-05 22:26:00 -0400

    try to reduce lag

--- script.js
+++ script.js
@@ -92,24 +92,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	for (let i = 0; i < numBgStars; i++) {
 		const star = document.createElement('div');
-		star.className = 'star';
+		star.className = 'starry';
 		const size = 5;
 		star.style.width = `${size}px`;
 		star.style.height = `${size}px`;
 		star.style.left = `${Math.random() * 100}vw`;
 		star.style.top = `${Math.random() * 100}vh`;
-
-		const moveX = (Math.random() - 0.5) * 100;
-		const moveY = (Math.random() - 0.5) * 100;
-		star.style.setProperty('--move-x', `${moveX}px`);
-		star.style.setProperty('--move-y', `${moveY}px`);
-
-		star.style.animationDuration = `${Math.random() * 2 + 1}s, ${Math.random() * 5 + 5}s`;
-		star.style.animationDelay = `${Math.random() * 2}s, ${Math.random() * 5}s`;
-		star.style.animationName = `twinkle, move`;
-		star.style.animationTimingFunction = 'ease-in-out, linear';
-		star.style.animationIterationCount = 'infinite, infinite';
-		star.style.animationFillMode = 'forwards';
 
 		body.appendChild(star);
 	}
